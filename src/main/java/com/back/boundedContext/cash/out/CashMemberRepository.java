2af3657cdc8d14c8a4b70f6c3a3c9c27af37a783
Dlrjswn <lkj6387@gmail.com>
2025-12-26 10:43:07 +0900

    0025

--- src/main/java/com/back/boundedContext/cash/out/CashMemberRepository.java
+++ src/main/java/com/back/boundedContext/cash/out/CashMemberRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface CashMemberRepository extends JpaRepository<CashMember, Long> {
+public interface CashMemberRepository extends JpaRepository<CashMember, Integer> {
     Optional<CashMember> findByUsername(String username);
 }
